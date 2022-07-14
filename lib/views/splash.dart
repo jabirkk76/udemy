3cbd9c0f30a6bf479be162c7118aa2f8542aeaf1
jabirkk76@gmail.com <jabirkk76@gmail.com>
2022-07-13 22:42:35 -0700

    initial

--- lib/views/splash.dart
+++ lib/views/splash.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:udemy/home_view.dart';
+import 'package:udemy/views/home_view.dart';
 
 class splash extends StatefulWidget {
   const splash({Key? key}) : super(key: key);
